5ce230c7d1247bf7f2ee4fdf49bd29fb1cf13b22
Mohamed Hazem <100438594+MohamedHazem5@users.noreply.github.com>
2023-11-14 19:15:24 +0200

    Added External Login Configuration and Methods

--- Smarket/Extentions/IdentityServiceExtentions.cs
+++ Smarket/Extentions/IdentityServiceExtentions.cs
@@ -1,44 +1,72 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.Text;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
 using Smarket.DataAccess;
 using Smarket.Models;
 
-
 namespace Smarket.Extentions
 {
-	public static class IdentityServiceExtentions
-	{
-		public static IServiceCollection AddIdentityService(this IServiceCollection services, IConfiguration configuration)
-		{
-			services.AddIdentityCore<User>(option =>
-			{
-				option.Password.RequireNonAlphanumeric = false;
-				option.SignIn.RequireConfirmedEmail = false;
-				option.SignIn.RequireConfirmedPhoneNumber = false;
-			})
-			 .AddRoles<Role>()
-			 .AddRoleManager<RoleManager<Role>>()
-			 .AddSignInManager<SignInManager<User>>()
-			 .AddRoleValidator<RoleValidator<Role>>()
-			 .AddEntityFrameworkStores<ApplicationDbContext>()
-			 .AddDefaultTokenProviders();
-			services.AddAuthentication(options =>
-			{
-				options.DefaultScheme = IdentityConstants.ApplicationScheme;
-				options.DefaultAuthenticateScheme = IdentityConstants.ApplicationScheme;
-				options.DefaultChallengeScheme = IdentityConstants.ApplicationScheme;
+    public static class IdentityServiceExtensions
+    {
+        public static IServiceCollection AddIdentityServices(this IServiceCollection services,
+           IConfiguration config)
+        {
 
-			})
-			.AddCookie("Identity.Application")
-			.AddCookie("Identity.External")
-			.AddCookie("Identity.TwoFactorRememberMe")
-			.AddCookie("Identity.TwoFactorUserId");
+            services.AddIdentityCore<User>(opt =>
+            {
+                opt.Password.RequireNonAlphanumeric = false;
+                opt.Lockout.MaxFailedAccessAttempts = 5;
+                opt.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+            })
+                .AddRoles<Role>()
+                .AddRoleManager<RoleManager<Role>>()
+                .AddSignInManager<SignInManager<User>>()
+                .AddRoleValidator<RoleValidator<Role>>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
 
 
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = IdentityConstants.ApplicationScheme;
+                options.DefaultAuthenticateScheme = IdentityConstants.ApplicationScheme;
+                options.DefaultChallengeScheme = IdentityConstants.ApplicationScheme;
 
+            })
+            .AddCookie("Identity.Application")
+            .AddCookie("Identity.TwoFactorRememberMe")
+            .AddCookie("Identity.TwoFactorUserId")
+
+
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"])),
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                    };
+
+                })
+
+                .AddGoogle(options =>
+                {
+                    options.ClientId = config["Authentication:Google:ClientId"];
+                    options.ClientSecret = config["Authentication:Google:ClientSecret"];
+                    options.CallbackPath = "/signin-google";
+                })
+                .AddMicrosoftAccount(options =>
+                {
+                    options.ClientId = config["Authentication:Microsoft:ClientId"];
+                    options.ClientSecret = config["Authentication:Microsoft:ClientSecret"];
+                    options.CallbackPath = "/signin-microsoft";
+                })
+                .AddCookie(IdentityConstants.ExternalScheme);
 
 
 
             return services;
-		}
-	}
+        }
+    }
 }
