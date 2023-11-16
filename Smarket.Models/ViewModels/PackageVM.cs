31e277b67791019fef48c6a3286ddba2abf44ba6
Mai allam <maimallam57@gmail.com>
2023-11-16 13:04:47 +0200

    PackageController

---
+++ Smarket.Models/ViewModels/PackageVM.cs
@@ -0,0 +1,43 @@
+﻿
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace Smarket.Models.ViewModels
+{
+    public class PackageVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public double ListPrice { get; set; } = 00;
+        public DateTime Date { get; set; } = DateTime.UtcNow.Date;
+        public DateTime ExpireDate { get; set; }
+        public int Stock { get; set; } = 0;
+
+        [Range(1, 1000)]
+        public double Price { get; set; } = 00;
+        public int ProductId { get; set; }
+
+        [ValidateNever]
+        public Product Product { get; set; }
+        public int InventoryId { get; set; }
+
+        [ValidateNever]
+        public Inventory Inventory { get; set; }
+        public int CartItemId { get; set; }
+
+        [ValidateNever]
+        public CartItem CartItem { get; set; }
+        public int OrderItemId { get; set; }
+
+        [ValidateNever]
+        public OrderItem OrderItem { get; set; }
+
+        [ValidateNever]
+        public IEnumerable<Inventory> Inventories { get; set; }
+
+        [ValidateNever]
+        public IEnumerable<Product> Products { get; set; }
+
+    }
+}
---
+++ Smarket/Controllers/PackageController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Smarket.DataAccess.Repository.IRepository;
+using Smarket.Models;
+using Smarket.Models.ViewModels;
+
+namespace Smarket.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PackageController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public PackageController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet("Get")]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var Packages = await _unitOfWork.Package.GetAllAsync();
+                return Ok(Packages);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            
+        }
+
+        [HttpPost("Create")]
+        public async Task<IActionResult> Create(PackageVM viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                viewModel.Inventories = await _unitOfWork.Inventory.GetAllAsync();
+                viewModel.Products = await _unitOfWork.Product.GetAllAsync();
+
+                Package package = new()
+                {
+                    Date = viewModel.Date,
+                    ExpireDate = viewModel.ExpireDate,
+                    Stock = viewModel.Stock,
+                    Price = viewModel.Price,
+                    ListPrice = viewModel.ListPrice,
+                    ProductId = viewModel.ProductId,
+                    InventoryId = viewModel.InventoryId,
+                    CartItemId = viewModel.CartItemId,
+                    OrderItemId = viewModel.OrderItemId
+                };
+
+                await _unitOfWork.Package.AddAsync(package);
+                await _unitOfWork.Save();
+                return Ok(viewModel);
+            }
+            else
+                return NotFound();
+        }
+
+
+        [HttpDelete("Delete/{id}")]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            var obj = await _unitOfWork.Package.FirstOrDefaultAsync(u => u.Id == id);
+            if (obj == null)
+                return NotFound();
+            else
+            {
+                _unitOfWork.Package.Delete(obj);
+                await _unitOfWork.Save();
+                return Ok();
+            }
+        }
+
+        [HttpGet("Details/{id}")]
+        public async Task<IActionResult> Details(int? id)
+        {
+            var obj = await _unitOfWork.Package.FirstOrDefaultAsync(u => u.Id == id);
+            if (obj == null)
+                return NotFound();
+            else
+                return Ok(obj);
+        }
+
+        [HttpPost("Edit")]
+        public async Task<IActionResult> Edit(Package obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Package.Update(obj);
+                await _unitOfWork.Save();
+            }
+            return Ok(obj);
+        }
+
+    }
+}
