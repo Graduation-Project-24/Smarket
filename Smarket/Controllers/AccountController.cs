1e7a96546a6547df833faddecc58678d356e5c53
Mohamed Hazem <100438594+MohamedHazem5@users.noreply.github.com>
2023-12-03 22:21:47 +0200

    Add EditUserProfile

--- Smarket/Controllers/AccountController.cs
+++ Smarket/Controllers/AccountController.cs
@@ -1,11 +1,11 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Smarket.Models;
 using Smarket.Models.Dtos;
-using Smarket.Services;
 using Smarket.Services.IServices;
-using Stripe;
 using System.Security.Claims;
 
 namespace Smarket.Controllers
@@ -16,10 +16,10 @@ namespace Smarket.Controllers
         private readonly SignInManager<User> _signInManager;
         private readonly ITokenService _tokenService;
         private readonly IEmailService _emailService;
-        private readonly ImageService _imageService;
+        private readonly IImageService _imageService;
 
         public AccountController(ITokenService tokenService, UserManager<User> userManager,
-            SignInManager<User> signInManager, IEmailService emailService, ImageService imageService)
+            SignInManager<User> signInManager, IEmailService emailService, IImageService imageService)
         {
             _userManager = userManager;
             _signInManager = signInManager;
@@ -91,6 +91,232 @@ namespace Smarket.Controllers
                 UserName = user.UserName,
                 Token = await _tokenService.CreateToken(user),
             };
+        }
+
+       
+
+
+        [HttpPost("SendConfirmationEmail")]
+        public async Task<ActionResult> SendConfirmationEmail(string email)
+        {
+            var user = await _userManager.FindByEmailAsync(email);
+
+            if (user == null)
+            {
+                return BadRequest("User not found");
+            }
+
+            if (user.EmailConfirmed)
+            {
+                return BadRequest("Email is already confirmed.");
+            }
+
+            var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+
+            var confirmationLink = Url.Action(nameof(ConfirmEmail), "Account", new { token, email }, Request.Scheme);
+
+            await _emailService.EmailSender(email, "Email Confirmation", confirmationLink);
+
+            return Ok("Email sent");
+        }
+
+        [HttpPost("resend-confirmation-email")]
+        public async Task<IActionResult> ResendConfirmationEmail(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+            {
+                return BadRequest("Invalid email.");
+            }
+
+            var user = await _userManager.FindByEmailAsync(email);
+
+            if (user == null)
+            {
+                return NotFound("User not found.");
+            }
+
+            if (user.EmailConfirmed)
+            {
+                return BadRequest("Email is already confirmed.");
+            }
+
+            var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+
+            var confirmationLink = Url.Action(nameof(ConfirmEmail), "Account", new { token, email }, Request.Scheme);
+
+            await _emailService.EmailSender(email, "Email Confirmation", confirmationLink);
+
+            return Ok("Email confirmation link resent.");
+        }
+
+        [HttpGet("confirm-email")]
+        public async Task<IActionResult> ConfirmEmail(string token, string email)
+        {
+            if (string.IsNullOrWhiteSpace(token) || string.IsNullOrWhiteSpace(email))
+            {
+                return BadRequest("Invalid token or email.");
+            }
+
+            var user = await _userManager.FindByEmailAsync(email);
+
+            if (user == null)
+            {
+                return NotFound("User not found.");
+            }
+
+            var result = await _userManager.ConfirmEmailAsync(user, token);
+
+            if (result.Succeeded)
+            {
+                return Ok("EmailConfirmed");
+            }
+            else
+            {
+                return BadRequest("Email confirmation failed.");
+            }
+        }
+
+
+        [HttpPut]
+        [Route("EditUser")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public async Task<IActionResult> EditUser([FromForm] EditUserDto dto)
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound("User not found");
+            }
+
+            var image = await _imageService.AddPhotoAsync(dto.formFile);
+
+            var cloudImage = new Image
+            {
+                PublicId = image.PublicId,
+                Url = image.Url.ToString(),
+            };
+
+            user.UserName = dto.UserName;
+            user.FirstName = dto.FirstName;
+            user.LastName = dto.LastName;
+            user.DateOfBirth = dto.DateOfBirth;
+            user.Email = dto.Email;
+            user.Image = cloudImage;
+            user.PhoneNumber = dto.PhoneNumber;
+            user.City = dto.City;
+            user.State = dto.State;
+            user.EmailConfirmed = false; // Assuming you want to set EmailConfirmed to false during the update
+
+            // Update the user using the UserManager
+            var result = await _userManager.UpdateAsync(user);
+
+            if (result.Succeeded)
+            {
+                return Ok(new
+                {
+                    Name = dto.UserName,
+                    Email = dto.Email,
+                    Image = cloudImage.Url.ToString(),
+                });
+            }
+
+            // Handle the case where the update failed
+            return BadRequest("Failed to update user");
+        }
+
+
+
+
+        [HttpPost("forgot-password")]
+        public async Task<IActionResult> ForgotPassword(ForgotPasswordDto forgotPasswordDto)
+        {
+            var user = await _userManager.FindByEmailAsync(forgotPasswordDto.Email);
+
+            if (user == null)
+            {
+                return BadRequest("User not found.");
+            }
+
+            var token = await _userManager.GeneratePasswordResetTokenAsync(user);
+            var resetLink = Url.Action(nameof(ResetPassword), "Account", new { token, email = forgotPasswordDto.Email }, Request.Scheme);
+
+            // Send the reset password link to the user's email
+            await _emailService.EmailSender(forgotPasswordDto.Email, "Reset Your Password", resetLink);
+
+            return Ok("Reset password link sent to your email.");
+        }
+
+        [HttpPost("reset-password")]
+        public async Task<IActionResult> ResetPassword(string Email, string Token)
+        {
+            var user = await _userManager.FindByEmailAsync(Email);
+
+            if (user == null)
+            {
+                return BadRequest("User not found.");
+            }
+            var NewPassword = "123456a";
+            var result = await _userManager.ResetPasswordAsync(user, Token, NewPassword);
+
+            if (result.Succeeded)
+            {
+                return Ok("Password reset successful.");
+            }
+            else
+            {
+                return BadRequest("Password reset failed.");
+            }
+        }
+
+        [HttpPost("ChangePassword")]
+        public async Task<IActionResult> ChangePassword(ChangePasswordDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(model.Email);
+
+                if (user is null)
+                    return BadRequest("Email Not Found");
+
+                var result = await _userManager.ChangePasswordAsync(user, model.CurrentPassword, model.NewPassword);
+                if (result.Succeeded)
+                {
+                    return Ok();
+                }
+                return BadRequest(result.Errors);
+            }
+            return BadRequest(model);
+        }
+
+        [HttpDelete("Delete")]
+        public async Task<IActionResult> DeleteAccount(string username)
+        {
+            var user = await _userManager.FindByNameAsync(username);
+
+            if (user == null)
+            {
+                return NotFound("User not found");
+            }
+
+            var result = await _userManager.DeleteAsync(user);
+
+            if (result.Succeeded)
+            {
+                return NoContent();
+            }
+            else
+            {
+                return BadRequest(result.Errors);
+            }
+        }
+
+
+        [HttpPost]
+        [Route("Logout")]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return Ok();
         }
 
         [HttpGet("ExternalLogin")]
@@ -205,183 +431,6 @@ namespace Smarket.Controllers
             // Handle errors during external login process.
             // For simplicity, just redirect to returnUrl.
             return LocalRedirect(returnUrl); // Redirect to returnUrl
-        }
-
-
-        [HttpPost("SendConfirmationEmail")]
-        public async Task<ActionResult> SendConfirmationEmail(string email)
-        {
-            var user = await _userManager.FindByEmailAsync(email);
-
-            if (user == null)
-            {
-                return BadRequest("User not found");
-            }
-
-            if (user.EmailConfirmed)
-            {
-                return BadRequest("Email is already confirmed.");
-            }
-
-            var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-
-            var confirmationLink = Url.Action(nameof(ConfirmEmail), "Account", new { token, email }, Request.Scheme);
-
-            await _emailService.EmailSender(email, "Email Confirmation", confirmationLink);
-
-            return Ok("Email sent");
-        }
-
-        [HttpPost("resend-confirmation-email")]
-        public async Task<IActionResult> ResendConfirmationEmail(string email)
-        {
-            if (string.IsNullOrWhiteSpace(email))
-            {
-                return BadRequest("Invalid email.");
-            }
-
-            var user = await _userManager.FindByEmailAsync(email);
-
-            if (user == null)
-            {
-                return NotFound("User not found.");
-            }
-
-            if (user.EmailConfirmed)
-            {
-                return BadRequest("Email is already confirmed.");
-            }
-
-            var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-
-            var confirmationLink = Url.Action(nameof(ConfirmEmail), "Account", new { token, email }, Request.Scheme);
-
-            await _emailService.EmailSender(email, "Email Confirmation", confirmationLink);
-
-            return Ok("Email confirmation link resent.");
-        }
-
-        [HttpGet("confirm-email")]
-        public async Task<IActionResult> ConfirmEmail(string token, string email)
-        {
-            if (string.IsNullOrWhiteSpace(token) || string.IsNullOrWhiteSpace(email))
-            {
-                return BadRequest("Invalid token or email.");
-            }
-
-            var user = await _userManager.FindByEmailAsync(email);
-
-            if (user == null)
-            {
-                return NotFound("User not found.");
-            }
-
-            var result = await _userManager.ConfirmEmailAsync(user, token);
-
-            if (result.Succeeded)
-            {
-                return Ok("EmailConfirmed");
-            }
-            else
-            {
-                return BadRequest("Email confirmation failed.");
-            }
-        }
-
-
-
-
-
-        [HttpPost("forgot-password")]
-        public async Task<IActionResult> ForgotPassword(ForgotPasswordDto forgotPasswordDto)
-        {
-            var user = await _userManager.FindByEmailAsync(forgotPasswordDto.Email);
-
-            if (user == null)
-            {
-                return BadRequest("User not found.");
-            }
-
-            var token = await _userManager.GeneratePasswordResetTokenAsync(user);
-            var resetLink = Url.Action(nameof(ResetPassword), "Account", new { token, email = forgotPasswordDto.Email }, Request.Scheme);
-
-            // Send the reset password link to the user's email
-            await _emailService.EmailSender(forgotPasswordDto.Email, "Reset Your Password", resetLink);
-
-            return Ok("Reset password link sent to your email.");
-        }
-
-        [HttpPost("reset-password")]
-        public async Task<IActionResult> ResetPassword(string Email, string Token)
-        {
-            var user = await _userManager.FindByEmailAsync(Email);
-
-            if (user == null)
-            {
-                return BadRequest("User not found.");
-            }
-            var NewPassword = "123456a";
-            var result = await _userManager.ResetPasswordAsync(user, Token, NewPassword);
-
-            if (result.Succeeded)
-            {
-                return Ok("Password reset successful.");
-            }
-            else
-            {
-                return BadRequest("Password reset failed.");
-            }
-        }
-
-        [HttpPost("ChangePassword")]
-        public async Task<IActionResult> ChangePassword(ChangePasswordDto model)
-        {
-            if (ModelState.IsValid)
-            {
-                var user = await _userManager.FindByEmailAsync(model.Email);
-
-                if (user is null)
-                    return BadRequest("Email Not Found");
-
-                var result = await _userManager.ChangePasswordAsync(user, model.CurrentPassword, model.NewPassword);
-                if (result.Succeeded)
-                {
-                    return Ok();
-                }
-                return BadRequest(result.Errors);
-            }
-            return BadRequest(model);
-        }
-
-        [HttpDelete("Delete")]
-        public async Task<IActionResult> DeleteAccount(string username)
-        {
-            var user = await _userManager.FindByNameAsync(username);
-
-            if (user == null)
-            {
-                return NotFound("User not found");
-            }
-
-            var result = await _userManager.DeleteAsync(user);
-
-            if (result.Succeeded)
-            {
-                return NoContent();
-            }
-            else
-            {
-                return BadRequest(result.Errors);
-            }
-        }
-
-
-        [HttpPost]
-        [Route("Logout")]
-        public async Task<IActionResult> Logout()
-        {
-            await _signInManager.SignOutAsync();
-            return Ok();
         }
     }
 }
